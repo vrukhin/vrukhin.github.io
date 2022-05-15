8e147a234a0ae8e0f78dcf033d131d2003e6dcc7
Pavel Vrukhin <vrukhin@protonmail.com>
2022-05-15 23:46:50 +0300

    Добавлена подсветка иконки активной цветовой схемы

--- static/js/theme_toggle.js
+++ static/js/theme_toggle.js
@@ -1,7 +1,11 @@
 const themeChange = (isChecked) => {
     if (isChecked) {
         document.body.setAttribute('dark', '');
+        document.getElementById('dark-mode-icon').classList.add('active');
+        document.getElementById('light-mode-icon').classList.remove('active');
     } else {
         document.body.removeAttribute('dark');
+        document.getElementById('dark-mode-icon').classList.remove('active');
+        document.getElementById('light-mode-icon').classList.add('active');
     }
 }
